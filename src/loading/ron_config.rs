4c0d4200851e0458a9a93e89e3ac2993deb07767
jaynnn <873242707@qq.com>
2024-04-02 21:19:54 +0800

    del

--- src/loading/ron_config.rs
+++
@@ -1,7 +0,0 @@
-use std::fs::read_to_string;
-use bevy_reflect::TypeRegistry;
-
-async fn load_configs<'a, P: AsRef<Path>>(path: P) -> Result<Vec<EntityConfig>, Box<dyn std::error::Error>> {
-    let content = read_to_string(path).await?;
-    ron::de::from_str(&content)
-}
